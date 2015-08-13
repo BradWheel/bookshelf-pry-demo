8e38eb93a5bae4a3712ade5a8a75a7c52716f394
bradwheel <bradley.m.wheel@gmail.com>
2015-08-13 17:08:45 -0400

    remove before_session default hook

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,9 @@ require_relative '../lib/bookshelf'
 require_relative '../apps/web/application'
 
 
+Pry.hooks.delete_hook(:before_session, :default)
+
+
 Lotus::Container.configure do
   mount Web::Application, at: '/'
 end
